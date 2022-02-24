eed29beefc8b8affc01064ba2ca13b5d0ac92945
Jhonry <jcopinaldo@gmail.com>
2022-02-24 16:17:01 -0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>Vita's Kitchen</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <section>
+      <p>We cook the most delicious food ever.</p>
+    </section>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>It's unbelieavble.</p>
+    </footer>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
-      <h2>Main</h2>
-      <h3>Footer</h3>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
