dde3a0c199c4dda313a34d6e04de6fa592588429
Jhonry <jcopinaldo@gmail.com>
2022-02-28 16:47:45 -0700

    adding how to display iamges

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import restaurant from "./restaurant.jpg";
 
 function Header(props) {
   return (
@@ -13,6 +14,11 @@ function Main(props) {
   return (
     <section>
       <p>We cook the most {props.adjective} food ever.</p>
+      <img 
+      src ={restaurant} 
+      height={200}
+      alt="image of a Milpitas restaurant"
+      />
       <ul style={{textAlign: "left"}}>
         {props.dishes.map((dish) => (
         <li key={dish.id}>{dish.title}</li>
